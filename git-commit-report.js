679fdf8cfc0d50a9a342d491529421b620fb9b48
priyank <priyank@example.com>
2025-04-30 00:01:59 +0400

    Refactoring related to processing of command line arguments

--- git-commit-report.js
+++ git-commit-report.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { execSync } = require('child_process');
+const path = require('node:path');
+const { execSync } = require('node:child_process');
+const { parseArgs } = require('node:util');
 
 const chalk = require('chalk');
 
 const scriptName = path.basename(process.argv[1]);
 
+// Show help information
 console.log(chalk.gray('\nFormat:   ' + scriptName + ' [--verbose] [commit-identifier (default: HEAD)]'));
 console.log(chalk.gray('Examples: ' + scriptName));
 console.log(chalk.gray('          ' + scriptName + ' HEAD~4'));
@@ -15,26 +17,31 @@ console.log(chalk.gray('          ' + scriptName + ' a0c98cd'));
 console.log(chalk.gray('          ' + scriptName + ' --verbose v1.0.0'));
 console.log('');
 
-const args = Array.from(process.argv);
-args.shift();
-args.shift();
+// Parse command line arguments using Node.js native utility
+const options = {
+    options: {
+        verbose: {
+        type: 'boolean',
+            default: false
+        }
+    }
+};
 
-let verbose = false;
-if (args[0] === '--verbose') {
-    args.shift();
-    verbose = true;
-}
+const { values, positionals } = parseArgs({
+    args: process.argv.slice(2),
+    options: options.options,
+    allowPositionals: true
+});
 
-if (!args.length) {
-    args.push('HEAD');
-}
+const verbose = values.verbose;
+let commitIdentifiers = positionals.length > 0 ? positionals : ['HEAD'];
 
 const naturalSort = (a, b) => {
     return a.localeCompare(b, undefined, { numeric: true });
 };
 
-for (let i = 0; i < args.length; i++) {
-    const commitIdentifier = args[i];
+for (let i = 0; i < commitIdentifiers.length; i++) {
+    const commitIdentifier = commitIdentifiers[i];
 
     // Block - Tags
     {
